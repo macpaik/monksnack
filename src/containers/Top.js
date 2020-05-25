3fe1d7430fc63d689ff9f776f6024a7bddde0175
macxpaik <macxpaik@gmail.com>
2020-05-25 23:21:43 +0900

    added section4

--- src/containers/Top.js
+++ src/containers/Top.js
@@ -38,6 +38,31 @@ const Top = () => {
           </p>
         </span>
       </div>
+      <div className={styles.Section4}>
+        <div className="row">
+          <div className='col'>
+            <img
+              src={dose_juice}
+              style={{'width': '100%'}}
+            />
+          </div>
+          <div className={[styles.Details, 'NanumSquareR', 'Normal', 'col'].join (' ')}>
+            <p>“어제 운동한 거 생각해서라도 참자...”</p>
+            <p>“다이어트 중이라 먹을 만한 게 없네...”</p>
+            <p>“아, 잘 참았는데 망했네. 모르겠다 그냥 먹자 ...”</p>
+            <p>“오늘까지만 먹고 내일부터 다이어트해야지...”</p>
+            <p className={['NanumSquareEB', 'Regular'].join (' ')}>다이어트 ... 평생의 과제</p>
+            <p>그런데 왜 굶는 다이어트를 하시나요?</p>
+            <p>왜 무조건 적으로 참으시려고 하나요?</p>
+            <p className={['NanumSquareEB', 'Small'].join (' ')}><b>이런 방식은 폭식으로 이어집니다.</b></p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col NanumSquareB Regular m-3">
+            <p>몽크스낵을 통해 폭식을 방지하고 건강한 식습관을 도모해보세요!</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
